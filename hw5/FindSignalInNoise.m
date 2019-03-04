f7fb67d97f56aa15592b00dcea35d5aaf0b81617
cameron.w.sullivan <cameron.w.sullivan@maine.edu>
2019-03-04 12:52:18 -0500

    HW5: fixed fit code

--- hw5/FindSignalInNoise.m
+++ hw5/FindSignalInNoise.m
@@ -4,31 +4,6 @@
 % has two columns. The first column is a time vector; the second is
 % experimentally gathered data.
 %
-% Once you have 'data' loaded, you can plot the function using
-
-% Hanselman's .mat file automatically create variables, so I do not assign the load result to anything!
-close all;
-clc;
-
-load('ECE498Data.mat');
-
-t = data(:,1);
-y = data(:,2);
-disp("Length of input data: " + length(t));
-
-plot(t,y);
-hold on;
-
-fun = @(x,t,y) norm(x(1)*cos(x(2)*t + x(3)) + x(4) - y);
-fun1 = @(x) fun(x,t,y);
-
-options = [];
-x0 = [0,0,0,0];
-x = fminsearch(fun1,x0,options)
-
-y_fit = x(1)*cos(x(2)*t + x(3)) + x(4) - y;
-plot(t,y_fit);
-
 % This week's task is to find the values of A, omega, theta and B that best
 % fits the data where
 %
@@ -46,3 +21,52 @@ plot(t,y_fit);
 % Use this script M-file to do your work.
 % Useful reading: 32.12 Nonlinear Curve Fitting
 % Useful functon: fminsearch
+
+close all;
+clc;
+
+% Hanselman's .mat file automatically create variables, so I do not assign the load result to anything!
+load('ECE498Data.mat');
+
+t = data(:,1);
+y = data(:,2);
+disp("Length of input data: " + length(t));
+
+fun = @(x,t,y) norm(x(1)*cos(x(2)*t + x(3)) + x(4) - y);
+fun1 = @(x) fun(x,t,y);
+
+options = [];
+
+% Default 0 guess.
+% x0 = [0,0,0,0];
+% First rough guess from looking at data.
+% x0 = [7,384,0,4];
+% Second guess based on output from first guess.
+x0 = [6.0962, 377.9243, -0.017609, 3.1543];
+
+x = fminsearch(fun1,x0,options);
+disp("A: " + x(1));
+disp("B: " + x(2));
+disp("C: " + x(3));
+disp("D: " + x(4));
+
+y_fit = x(1)*cos(x(2)*t + x(3)) + x(4);
+
+% Plot original signal and cosine fit signal.
+subplot(2,1,1);
+plot(t,y,t,y_fit,'linewidth',2);
+set(gca,'fontsize',20);
+title('Approximation of a Noisy Signal with Fminsearch');
+ylabel('Value');
+xlabel('Time');
+legend('Raw data', 'Cosine fit');
+
+% Plot error.
+subplot(2,1,2);
+plot(t,y-y_fit,'linewidth',2);
+set(gca,'fontsize',20);
+title('Fit Error versus Time');
+ylabel('Fit error');
+xlabel('Time');
+
+% NEED TO ADD ERROR NORM MEASUREMENT AS SHOWN IN BOOK
