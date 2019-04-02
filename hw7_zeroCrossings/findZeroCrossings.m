83e7831325c3fdaac573719a5359431105fb5db5
cameron.w.sullivan <cameron.w.sullivan@maine.edu>
2019-04-02 17:35:01 -0400

    HW7: Hanselman's method - final approximation

--- hw7_zeroCrossings/findZeroCrossings.m
+++ hw7_zeroCrossings/findZeroCrossings.m
@@ -31,26 +31,23 @@
 
 % do your work in this M file.
 
-close all;
+% close all;
 clc;
 
-% Find zero crossings
+% Create an inline function based on Hanselman's function. This can be passed in to fzero().
 fun = @zerocrossings;
 
-% x = linspace(-10,10,500);
-x = linspace(-10, 10, 10000);
-zero_func = zeros(10000, 1);
-y = zerocrossings(x);
+x1 = linspace(-10, -4.15, 500);
+y1 = zerocrossings(x1);
+x_discontinuity = linspace(-4.15, -4.1, 1000);
+y_discontinuity = zerocrossings(x_discontinuity);
+x2 = linspace(-4.1, 10, 500);
+y2 = zerocrossings(x2);
 
 xzero(1) = fzero(fun, -4.549);
-
-subplot(2,1,1);
-plot(x, zero_func, 'k');
-hold on;
 % fzero() provides the correct x-value, but wrong y-value.
 % The y-value seems to be a continuation of the exponential line.
 xzero(2) = fzero(fun, -4.124);
-
 xzero(3) = fzero(fun, -3.507);
 xzero(4) = fzero(fun, -2.826);
 xzero(5) = fzero(fun, 2.385);
@@ -60,10 +57,16 @@ xzero(8) = fzero(fun, 3.828);
 xzero(9) = fzero(fun, 5.07)
 yzero = zerocrossings(xzero)
 
-% subplot(2,1,1);
+subplot(2,1,1);
 % plot(x,y);
 % scatter(x,y)
-plot(x,y,xzero,yzero,'ko')
+x_zero = linspace(-10, 10, 1000);
+zero_func = zeros(1000, 1);
+plot(x_zero, zero_func, 'k');
+hold on;
+plot(x_discontinuity, y_discontinuity);
+plot(xzero,yzero,'ko');
+% plot(x1,y1,'b',x_discontinuity,y_discontinuity,'b',x2,y2,'b',xzero,yzero,'ko')
 
 % subplot(2,1,2);
 % plot(xzero,yzero,'o');
