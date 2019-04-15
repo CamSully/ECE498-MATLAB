c0d916e2c7be6800539a8d86ded642e856342da0
cameron.w.sullivan <cameron.w.sullivan@maine.edu>
2019-04-14 21:40:07 -0400

    HW9 ready for demo

--- hw9_circShift/circshift_test.m
+++ hw9_circShift/circshift_test.m
@@ -3,11 +3,40 @@
 clc;
 clear;
 
-mycircshift([1 2],1)
+% Test error checking functionality.
+% Test differing numbers of input arguments.
+% mycircshift()
+% mycircshift([1 2 3])
+% mycircshift([1 2], 1, 2)
+
+% Test A values.
+% mycircshift([1 'a'], 1)
+% mycircshift([nan nan], 1)
+% mycircshift([2 + 3i 1], 1)
+% mycircshift([999999999999999999999999 99999999999999999999999], 1)
+
+% Test K values.
+% mycircshift([1 2], [])
+% mycircshift([1 2], [1 2])
+% mycircshift([1 2], 'a')
+% mycircshift([1 2], nan)
+% mycircshift([1 2], 2 + 3i)
+% mycircshift([1 2], 0.5)
+% mycircshift([1 2], 3.79)
+% mycircshift([1 2], 9999999999999999)
+
+
+
+% Test 1D arrays
 % mycircshift([1 2 3 4 5], 2)
 % mycircshift([1 2 3 4 5], -4)
 
+% Test 2D arrays
 % twoD_array = [1 2 3; 4 5 6; 7 8 9];
 % mycircshift(twoD_array, 1)
 % mycircshift(twoD_array, [1 -1])
 % mycircshift(twoD_array, [0 1])
+
+% Test a non-square array.
+% myArray = [2 5; 3 1; 4 8]
+% mycircshift(myArray, [1 1])
--- hw9_circShift/mycircshift.m
+++ hw9_circShift/mycircshift.m
@@ -40,41 +40,53 @@ function Y = mycircshift(A,K)
 %        9  7  8
 
 % Sullivan, Cameron
-% 2019/4/Day
+% 2019/4/14
 
 % Be sure to include appropriate error checking on the input arguments
 % Are they all there? Do they have appropriate dimensions and acceptable
 % values?
 % use the MATLAB function circshift to check your results
 % no loops allowed
-
 % dimensions do not change, i.e., size(Y) = size(A)
+
+
 
 % User must provide exactly two arguments.
 if nargin == 0 || nargin == 1 || nargin > 2
-    error('Not enough input arguments provided. Two arguments are required...');
+    error('Unacceptable number of input arguments provided. Two arguments are required...');
 end
 
 % Array error checking.
 if isempty(A) || length(A) == 1
     error('Input matrix A must have at least two elements.');
 end
-if isnan(A) || ~isnumeric(A) || ~isreal(A) || fix(A) ~= A || A > flintmax
-    error('Input value must be a real, numeric, integer...')
+if isnan(A) | ~isnumeric(A) | ~isreal(A) | A > flintmax
+    error('Array "A" must only contain numeric, real data...')
 end
 
 % Shift value error checking.
-if isnan(K) || ~isscalar(K) || ~isnumeric(K) || ~isreal(K) || K < 1 || fix(K) ~= K || K > flintmax
-    error('Input value must be a real, positive, numeric, integer...')
+if isempty(K) || length(K) > 2
+    error('K must be 1 or 2 elements long.');
+end
+if isnan(K) | ~isnumeric(K) | ~isreal(K) | fix(K) ~= K | K > flintmax
+    error('Shift value "K" must be a real, positive, numeric, integer...')
 end
 
+
+
+% Get number of rows & columns in A.
 A_size = size(A);
 num_rows = A_size(1);
 num_cols = A_size(2);
 
 
-% 1D ARRAY
+
+% 1D array circular shifting.
 if (num_rows == 1)
+    % Ensure that K only contains a single element for a row vector input.
+    if length(K) > 1
+        error('K must only contain a single scalar for row vector input.')
+    end
 %     If the user attempts to shift a length further than the length of the array, 
 %     decrease the shift value to be less than the array.
 %     This will cause an equivalent shifting operation.
@@ -85,36 +97,44 @@ if (num_rows == 1)
         K = K - length(A);
     end
 
-    % Allocate space for the shifted array.
+    % Allocate space for the returned output array.
     Y = zeros(1, length(A));
 
-    % Get the indices of the input array.
+    % Get the indices of the 1D input array.
     indices = 1:length(A);
     % Add the shift number to each index.
-    new_indices = indices + K;
+    shifted_indices = indices + K;
     % If the new index is below 1, circularly shift it to the end of the array.
-    new_indices(new_indices < 1) = new_indices(new_indices < 1) + length(A);
+    shifted_indices(shifted_indices < 1) = shifted_indices(shifted_indices < 1) + length(A);
     % If the new index is above the max index, circularly shift it to the beginning of the array.
-    new_indices(new_indices > length(A)) = new_indices(new_indices > length(A)) - length(A);
+    shifted_indices(shifted_indices > length(A)) = shifted_indices(shifted_indices > length(A)) - length(A);
     % Create the shifted array by placing the original A values in the new indices.
-    Y(new_indices) = A(indices);
+    Y(shifted_indices) = A(indices);
     return
 end
 
 
-% 2D ARRAY
+
+% 2D array circular shifting.
 if (num_rows > 1)
+    % Complete the same process as for 1D arrays, except that rows and columns are evaluated separately.
     row_indices = 1:num_rows;
+    % Shift rows by the value at the first index of K.
     new_row_indices = row_indices + K(1);
     new_row_indices(new_row_indices < 1) = new_row_indices(new_row_indices < 1) + num_rows;
     new_row_indices(new_row_indices > num_rows) = new_row_indices(new_row_indices > num_rows) - num_rows;
+    % Shift the rows of A into the new rows of Y.
     Y(new_row_indices,:) = A(row_indices,:);
 
+    % If the user entered an array of two values for K, shift the columns by K(2).
     if (length(K) == 2)
+        % Complete the same process as for rows, but shift columns instead.
         col_indices = 1:num_cols;
+        % Shift columns by the value at the second index of K.
         new_col_indices = col_indices + K(2);
         new_col_indices(new_col_indices < 1) = new_col_indices(new_col_indices < 1) + num_cols;
         new_col_indices(new_col_indices > num_cols) = new_col_indices(new_col_indices > num_cols) - num_cols;
+        % Shift the columns of the original Y output into the final Y output.
         Y(:,new_col_indices) = Y(:,col_indices);
     end
     return;
